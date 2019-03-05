1aa391490fb46aa66ff64337ee74663d899d35f6
Pierrick <deshayes.pierrick@hotmail.fr>
2019-03-05 16:39:25 +0100

    Config

--- src/main/java/org/sid/security/Role.java
+++ src/main/java/org/sid/security/Role.java
@@ -2,16 +2,16 @@ package org.sid.security;
 
 public enum Role {
 
-	USER("User"), ADMIN("Admin");
+    USER("User"), ADMIN("Admin");
 
-	private final String name;
+    private final String name;
 
-	private Role(String name) {
-		this.name = name;
-	}
+    private Role(String name) {
+        this.name = name;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
 }
