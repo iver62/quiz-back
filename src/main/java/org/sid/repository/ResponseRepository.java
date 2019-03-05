627882068c9b719d5109b3c774bc5212eb69727a
Pierrick <deshayes.pierrick@hotmail.fr>
2019-03-05 16:41:53 +0100

    Deletions

--- src/main/java/org/sid/repository/ResponseRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sid.repository;
-
-import org.sid.entities.Response;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResponseRepository extends JpaRepository<Response, Long> {
-
-}
--- src/main/java/org/sid/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.sid.repository;
-
-import org.sid.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
